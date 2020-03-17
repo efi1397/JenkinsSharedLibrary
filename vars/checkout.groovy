ce0f4fa0c527a8e3fe3f852f3065dad67a82ceb9
Effi Tzaig <efi1397@gmail.com>
2020-03-17 23:39:46 +0200

    test

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,8 +1,5 @@
-def call(serverLabel) {
-    def label = serverLabel ?: "windows"
-    node(label) {
-        //def scmVars = checkout scm
-        def scmVars = checkout([$class: 'GitSCM', branches: [[name: '*/dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'fa992e88-b598-4c23-ad84-5126a3ad39f1', url: 'https://gitlab.com/Cmenow/jenkinssharedlibrary.git']]])
-        println(scmVars.toString())
+def call() {
+    node {
+        checkout scm
     }
 }
