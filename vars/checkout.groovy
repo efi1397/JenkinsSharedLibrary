9ec017d4e45c20a58efad6fbe2c00d076e18317e
Effi Tzaig <efi1397@gmail.com>
2020-03-18 11:07:11 +0200

    create new method

--- vars/checkout.groovy
+++
@@ -1,7 +0,0 @@
-def call() {
-    node('windows') {
-        echo("checkout: ")
-        def scmDetails = checkout([$class: 'GitSCM', branches: [[name: '*/dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'fa992e88-b598-4c23-ad84-5126a3ad39f1', url: 'https://gitlab.com/Cmenow/jenkinssharedlibrary.git']]])
-        echo(scmDetails.toString())
-    }
-}
