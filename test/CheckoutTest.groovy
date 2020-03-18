ab3dc66e52da86c897c02ef0eab03a35e98f7627
Effi Tzaig <efi1397@gmail.com>
2020-03-18 17:56:59 +0200

    Create unitTest

---
+++ test/CheckoutTest.groovy
@@ -0,0 +1,25 @@
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Test
+
+class CheckoutTest extends BasePipelineTest {
+    @Override
+    @Before
+    void setUp() {
+        super.setUp()
+        binding.setVariable('scm', [$class: 'GitSCM', branches: [[name: '*/dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/efi1397/JenkinsSharedLibrary.git']]])
+        helper.registerAllowedMethod('checkout', [String.class], null)
+
+    }
+
+// when i do push linkting
+// create pipeline for jenkinsFile
+
+    @Test
+    void testCall() {
+        // create mock checkout step
+        def checkoutTest = loadScript("vars/checkoutScm.groovy")
+        checkoutTest.call()
+        printCallStack()
+    }
+}
--- vars/checkoutScm.groovy
+++ vars/checkoutScm.groovy
@@ -2,6 +2,8 @@ def call() {
     node('windows') {
         echo("checkout: ")
         def scmDetails = checkout scm
+        echo(scm.toString())
         echo(scmDetails.toString())
+        return scmDetails
     }
 }
